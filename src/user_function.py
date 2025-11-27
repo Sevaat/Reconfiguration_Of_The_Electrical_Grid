ffe8bb0e1b219d5062fb0b8093d0b48c4b2ad4a2
tkachenko <sevaatmail@gmail.com>
2025-11-27 17:42:40 +0500

    Исправлена пользовательская функция

--- src/user_function.py
+++ src/user_function.py
@@ -17,13 +17,13 @@ def get_fitness(genotype: List[str]) -> float:
     nm = NewtonMethod()
     branches: List[Branch] = []
     for branch in nm.branches:
-        if branch.start == "4" and branch.end == "5" and genotype[0] == "0":
+        if branch.start.name == "4" and branch.end.name == "5" and genotype[0] == "0":
             continue
-        elif branch.start == "5" and branch.end == "6" and genotype[1] == "0":
+        elif branch.start.name == "5" and branch.end.name == "6" and genotype[1] == "0":
             continue
-        elif branch.start == "2" and branch.end == "5" and genotype[2] == "0":
+        elif branch.start.name == "2" and branch.end.name == "5" and genotype[2] == "0":
             continue
-        elif branch.start == "3" and branch.end == "6" and genotype[3] == "0":
+        elif branch.start.name == "3" and branch.end.name == "6" and genotype[3] == "0":
             continue
         else:
             branches.append(branch)
